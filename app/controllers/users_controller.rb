5c34e60532ff23dce48a871bd3b62871440d08d4
garv123 <garv.mathur@gmail.com>
2015-05-10 00:35:43 +0530

    Created User And Micropost

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
